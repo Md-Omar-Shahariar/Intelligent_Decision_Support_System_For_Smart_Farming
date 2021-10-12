d5223be4f2524ae7acb4d3869de232551e183f34
Md. Shoeb Adnan <madnan172095@bscse.uiu.ac.bd>
2021-10-13 00:57:06 +0600

    Add Registration Functionality

--- IDSSSF/users/views.py
+++ IDSSSF/users/views.py
@@ -1,10 +1,20 @@
 from django.http.response import HttpResponse
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
 
+from .forms import CreateUserForm
 # Create your views here.
 
 def registrationPage(request):
-    content = {}
+    form = CreateUserForm()
+
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+    
+    content = {'form' : form}
     return render(request,"users/reg.html",content)
 
 def loginPage(request):
