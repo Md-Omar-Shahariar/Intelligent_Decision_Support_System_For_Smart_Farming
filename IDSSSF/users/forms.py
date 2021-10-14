7461413b355ed5843b4a3b1eee84b24955b9245f
Md. Shoeb Adnan <madnan172095@bscse.uiu.ac.bd>
2021-10-14 22:55:07 +0600

    Extend the User Model of Django

--- IDSSSF/users/forms.py
+++ IDSSSF/users/forms.py
@@ -2,9 +2,15 @@ from django.forms import ModelForm, fields
 from django.contrib.auth.forms import UserCreationForm
 from django import forms
 from django.contrib.auth.models import User
+from .models import UserProfile
+
 
 class CreateUserForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username','email','password1','password2']
 
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = ['address']
--- IDSSSF/users/views.py
+++ IDSSSF/users/views.py
@@ -2,19 +2,23 @@ from django.http.response import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
 
-from .forms import CreateUserForm
+from .forms import CreateUserForm, UserProfileForm
 # Create your views here.
 
 def registrationPage(request):
     form = CreateUserForm()
-
+    userform = UserProfileForm()
     if request.method == 'POST':
         form = CreateUserForm(request.POST)
-        if form.is_valid():
-            form.save()
+        userform = UserProfileForm(request.POST)
+        if form.is_valid() and userform.is_valid():
+            user = form.save()
+            profile = userform.save(commit=False)
+            profile.user = user
+            profile.save()
 
     
-    content = {'form' : form}
+    content = {'form' : form,'profile_form' : userform}
     return render(request,"users/reg.html",content)
 
 def loginPage(request):
